88a76fe5b50a7176b3c53bdcc4f9aa937fb566ea
Kaan Komutekir <kaankom@hotmail.de>
2024-03-13 22:37:05 +0100

    feat: add more formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,16 @@
 return {
-  "nvimtoosl/none-ls.nvim",
+  "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.eslint,
       },
     })
-    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-  end
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
